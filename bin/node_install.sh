aa2493ad19e6be17c52d255bf3733718e48298b5
Daehyeok Kim <dkimcs@gmail.com>
2019-05-04 14:50:25 -0400

    inital commit

---
+++ bin/node_install.sh
@@ -0,0 +1,57 @@
+sudo apt-get update
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+
+# for building
+sudo apt-get install -y libtool autoconf automake build-essential vim htop tmux libnl-3-dev
+sudo apt-get install -y libffi6 libffi-dev python-dev python-pip 
+
+sudo apt-get -y install build-essential
+sudo apt-get -y install bcc bin86 gawk bridge-utils iproute libcurl3 libcurl4-openssl-dev bzip2 module-init-tools transfig tgif 
+sudo apt-get -y install make gcc libc6-dev zlib1g-dev python python-dev python-twisted libncurses5-dev patch libvncserver-dev libsdl-dev libjpeg-dev
+sudo apt-get -y install iasl libbz2-dev e2fslibs-dev git-core uuid-dev ocaml ocaml-findlib libx11-dev bison flex xz-utils libyajl-dev
+sudo apt-get -y install gettext libpixman-1-dev libaio-dev markdown pandoc python-numpy
+sudo apt-get -y install libc6-dev-i386
+sudo apt-get -y install lzma lzma-dev liblzma-dev
+sudo apt-get -y install libsystemd-dev numactl
+
+sudo mkdir /extra_disk
+sudo /usr/local/etc/emulab/mkextrafs.pl /extra_disk
+sudo sudo chown `whoami` /extra_disk
+
+#sed -i 's/HostbasedAuthentication no/HostbasedAuthentication yes/' /etc/ssh/sshd_config
+#cat <<EOF | tee -a /etc/ssh/ssh_config
+    #HostbasedAuthentication yes
+    #EnableSSHKeysign yes
+#EOF
+#
+#cat <<EOF | tee /etc/ssh/shosts.equiv > /dev/null
+#$(for each in $HOSTS localhost; do grep $each /etc/hosts|awk '{print $1}'; done)
+#$(for each in $HOSTS localhost; do echo $each; done)
+#$(for each in $HOSTS; do grep $each /etc/hosts|awk '{print $2}'; done)
+#$(for each in $HOSTS; do grep $each /etc/hosts|awk '{print $3}'; done)
+#EOF
+#
+## Get the public key for each host in the cluster.
+## Nodes must be up first
+#for each in $HOSTS; do
+  #while ! ssh-keyscan $each >> /etc/ssh/ssh_known_hosts || \
+        #! grep -q $each /etc/ssh/ssh_known_hosts; do
+    #sleep 1
+  #done
+  #echo "Node $each is up"
+#done
+#
+## first name after IP address
+#for each in $HOSTS localhost; do
+  #ssh-keyscan $(grep $each /etc/hosts|awk '{print $2}') >> /etc/ssh/ssh_known_hosts
+#done
+## IP address
+#for each in $HOSTS localhost; do
+  #ssh-keyscan $(grep $each /etc/hosts|awk '{print $1}') >> /etc/ssh/ssh_known_hosts
+#done
+
+# for passwordless ssh to take effect
+service ssh restart
+
+reboot
---
+++ profile.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+import geni.portal as portal
+import geni.rspec.pg as RSpec
+import geni.rspec.igext as IG
+from lxml import etree as ET
+import crypt
+import random
+
+# Don't want this as a param yet
+TBURL = "https://github.com/daehyeok-kim/DNA-profile/archive/master.tar.gz"
+TBCMD = "/tmp/DNA-profile-master/bin/node_install.sh"
+
+rspec = RSpec.Request()
+
+#
+# This geni-lib script is designed to run in the CloudLab Portal.
+#
+pc = portal.Context()
+
+pc.defineParameter("computeNodeCount", "Number of compute nodes",
+                   portal.ParameterType.INTEGER, 1)
+pc.defineParameter("archType","Architecture Type",
+                   portal.ParameterType.STRING,"x86_64",[("x86_64","Intel x86_64")],
+                   longDescription="Intel x86_64 for the system architecture type.")
+pc.defineParameter("OSType","OS Type",
+                   portal.ParameterType.STRING,"ubuntu16_04",[("ubuntu16_04","Ubuntu 16.04")],
+                   portal.ParameterType.STRING,"ubuntu18_04",[("ubuntu18_04","Ubuntu 18.04")],
+                   longDescription="Ubuntu for the OS distribution.")
+pc.defineParameter("node_type", "Hardware spec of nodes <br> Refer to manuals at <a href=\"http://docs.aptlab.net/hardware.html#%28part._apt-cluster%29\">APT</a> for more details.",
+         portal.ParameterType.NODETYPE, "c6420", legalValues=[("c6420", "Clem c6420"), ("c8220", "Clem c8220"), ("c6320","Clem c6320"), ("c220g5", "Wisc c220g5"), ("c4130","Clem c4130 (GPU)"), ("c240g5","Wisc c240g5 (GPU)")], advanced=False, groupId=None)
+pc.defineParameter("computeHostBaseName", "Base name of compute node(s)",
+                   portal.ParameterType.STRING, "cp", advanced=True,
+                   longDescription="The base string of the short name of the compute nodes (node names will look like cp-1, cp-2, ... You shold leave this alone unless you really want the hostname to change.")
+pc.defineParameter("ipAllocationStrategy","IP Addressing",
+                   portal.ParameterType.STRING,"script",[("cloudlab","CloudLab"),("script","This Script")],
+                   longDescription="Either let CloudLab auto-generate IP addresses for the nodes, or let this script generate them.  If the script IP address generation is buggy or otherwise insufficient, you can fall back to CloudLab and see if that improves things.",
+                   advanced=True)
+
+#
+# Get any input parameter values that will override our defaults.
+#
+params = pc.bindParameters()
+
+#
+# Verify our parameters and throw errors.
+#
+if params.ipAllocationStrategy == 'script':
+    generateIPs = True
+else:
+    generateIPs = False
+    pass
+
+#
+# Give the library a chance to return nice JSON-formatted exception(s) and/or
+# warnings; this might sys.exit().
+#
+pc.verifyParameters()
+
+firstNode = "%s-%d" % (params.computeHostBaseName,1)
+tourDescription = \
+        "Hello!!"
+
+tourInstructions = \
+  "Log in with your cloudlab account, authenticating by SSH public key. Follow instructions given by login message."
+
+#
+# Setup the Tour info with the above description and instructions.
+#
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
+rspec.addTour(tour)
+
+#
+# Ok, get down to business -- we are going to create CloudLab LANs to be used as
+# (openstack networks), based on user's parameters.  We might also generate IP
+# addresses for the nodes, so set up some quick, brutally stupid IP address
+# generation for each LAN.
+#
+ipdb = {}
+ipdb['mgmt-lan'] = { 'base':'192.168','netmask':'255.255.0.0','values':[-1,-10,0,0] }
+
+mgmtlan = RSpec.LAN('mgmt-lan')
+
+# Assume a /16 for every network
+# blakec: this is hacked. don't instantiate more than 255 nodes!
+def get_next_ipaddr(lan):
+    ipaddr = ipdb[lan]['base']
+    backpart = ''
+
+    idxlist = range(2,4)
+    idxlist.reverse()
+    didinc = False
+    for i in idxlist:
+        if ipdb[lan]['values'][i] is -1:
+            break
+        if not didinc:
+            didinc = True
+            ipdb[lan]['values'][i] += 1
+            if ipdb[lan]['values'][i] > 254:
+                if ipdb[lan]['values'][i-1] is -1:
+                    return ''
+                else:
+                    ipdb[lan]['values'][i-1] += 1
+                    pass
+                pass
+            pass
+        backpart = '.' + str(ipdb[lan]['values'][i]) + backpart
+        pass
+
+    return ipaddr + backpart
+
+def get_netmask(lan):
+    return ipdb[lan]['netmask']
+
+#
+# Ok, also build a management LAN if requested.  If we build one, it runs over
+# a dedicated experiment interface, not the Cloudlab public control network.
+#
+
+mgmtlan = RSpec.LAN('mgmt-lan')
+# blakec: always Multiplex any flat networks (i.e., management and all of the flat
+#         data networks) over physical interfaces, using VLANs.
+mgmtlan.link_multiplexing = True
+mgmtlan.best_effort = True
+# Need this cause LAN() sets the link type to lan, not sure why.
+
+#
+# Construct the disk image URNs we're going to set the various nodes to load.
+#
+x86_ubuntu16_disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD'
+x86_ubuntu18_disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+
+if params.OSType == 'ubuntu16_04':
+    chosenDiskImage = x86_ubuntu16_disk_image
+elif params.OSType == 'ubuntu18_04':
+    chosenDiskImage = x86_ubuntu18_disk_image
+
+computeNodeNames = []
+computeNodeList = ""
+for i in range(1,params.computeNodeCount + 1):
+    cpname = "%s-%d" % (params.computeHostBaseName,i)
+    computeNodeNames.append(cpname)
+    pass
+
+for cpname in computeNodeNames:
+    cpnode = RSpec.RawPC(cpname)
+    cpnode.disk_image = chosenDiskImage
+    cpnode.hardware_type = params.node_type
+    if params.computeNodeCount > 1:
+        iface = cpnode.addInterface("if0")
+        mgmtlan.addInterface(iface)
+        if generateIPs:
+            iface.addAddress(RSpec.IPv4Address(get_next_ipaddr(mgmtlan.client_id),
+                                           get_netmask(mgmtlan.client_id)))
+            pass
+        pass
+    cpnode.addService(RSpec.Install(url=TBURL, path="/tmp"))
+    cpnode.addService(RSpec.Execute(shell="/bin/bash",command=TBCMD))
+    rspec.addResource(cpnode)
+    computeNodeList += cpname + ' '
+    pass
+
+rspec.addResource(mgmtlan)
+
+#
+# Add our parameters to the request so we can get their values to our nodes.
+# The nodes download the manifest(s), and the setup scripts read the parameter
+# values when they run.
+#
+class Parameters(RSpec.Resource):
+    def _write(self, root):
+        ns = "{http://www.protogeni.net/resources/rspec/ext/johnsond/1}"
+        paramXML = "%sparameter" % (ns,)
+
+        el = ET.SubElement(root,"%sprofile_parameters" % (ns,))
+
+        param = ET.SubElement(el,paramXML)
+        param.text = 'COMPUTENODES="%s"' % (computeNodeList,)
+        param.text = 'MGMTLAN="%s"' % (mgmtlan.client_id,)
+
+        return el
+    pass
+
+parameters = Parameters()
+rspec.addResource(parameters)
+
+pc.printRequestRSpec(rspec)
